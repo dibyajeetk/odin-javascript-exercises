051c90547e355f6b9bf22b7d638e3d791155ad1e
dibyajeetk <dibyajeet.creates@gmail.com>
2025-06-25 10:13:04 +0530

    feat: Exercises fron loops and Arrays
    - Struggled quite a bit with logic
    - I was not able to solve 07 & 08
    
    dated: 25 June, 2025
    Time: 10:15 IST

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  raw = ((temp - 32) * 5/9);
+  return parseFloat(raw.toFixed(1));
+  // return Math.floor(raw)
+  // return raw.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  raw = ((temp * 9 / 5) + 32)
+  return parseFloat(raw.toFixed(1));
+  // return raw.toFixed(1)
 };
 
 // Do not edit below this line
